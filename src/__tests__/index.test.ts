46a1dc095ef02fc43b743f84027dcf09a4c636cd
akp111 <pradhanashish95@gmail.com>
2022-10-05 15:50:42 +0530

    added more functions

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,31 @@
-import { add } from '../index';
+import { add, sub, mul, div, mod, total, avg } from '../index';
 
 test('Test add function', () => {
   expect(add(1, 2)).toBe(3);
+});
+test('Test add function with decimal', () => {
+  expect(add(1.1, 2)).toBe(3.1);
+});
+test('Test sub function', () => {
+  expect(sub(1, 2)).toBe(-1);
+});
+
+test('Test mul function', () => {
+  expect(mul(10000000000000, 10000000000000)).toBe(100000000000000000000000000);
+});
+
+test('Test div function', () => {
+  expect(div(10000000000000, 10000000000000)).toBe(1);
+});
+
+test('Test mod function', () => {
+  expect(mod(10000000000000, 10000000000000)).toBe(0);
+});
+
+test('Test total function', () => {
+  expect(total([1, 1, 1, 1, 1])).toBe(5);
+});
+
+test('Test avg function', () => {
+  expect(avg([1, 1, 1, 1, 1])).toBe(1);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
 export const add = (x: number, y: number): number => {
   return x + y;
 };
+
+export const sub = (x: number, y: number): number => {
+  return x - y;
+};
+
+export const mul = (x: number, y: number): number => {
+  return x * y;
+};
+
+export const div = (x: number, y: number): number => {
+  return x / y;
+};
+
+export const mod = (x: number, y: number): number => {
+  return x % y;
+};
+
+export const total = (x: number[]): number => {
+  let sum = 0;
+  for (let i = 0; i < x.length; i++) sum += x[i];
+  return sum;
+};
+
+export const avg = (x: number[]): number => {
+  return total(x) / x.length;
+};
